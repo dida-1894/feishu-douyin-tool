7b5b52e2c99f59391f1db132d70bb2e667fc6fcc
weihua.xiao <weihua.xiao@viatris.com>
2024-01-31 20:28:04 +0800

    Revert "Revert "增加小红书接口""
    This reverts commit 719a88a034d5b1228f5aa285834fa60a3cde238f.

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var favicon = require('serve-favicon')
 
 var apiRouter = require('./routes/api');
+var redbookRouter = require('./routes/redbook');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.all('*', function (req, res, next) {
 app.use(express.static(path.join(__dirname, 'public')));
 // 调用api
 app.use('/api', apiRouter);
+app.use('/redbook', redbookRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
