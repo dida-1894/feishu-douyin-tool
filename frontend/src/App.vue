4a9422877b5dedc20acd19eb34866fcb868d2c40
weihua.xiao <weihua.xiao@viatris.com>
2024-02-22 22:12:33 +0800

    add 51la

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { onMounted } from 'vue'
 import Home from './components/Home.vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = "//js.users.51.la/21848355.js"
+  script.type = "text/javascript"
+  document.body.appendChild(script)
+})
 </script>
 
 <template>
